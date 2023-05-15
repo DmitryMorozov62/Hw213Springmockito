ce6abeedef85af9b0c21d85ea86bffb63e37a40e
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-05-15 18:36:56 +0300

    Initial commit

---
+++ src/main/java/com/example/hwspringmockito/Hw213SpringmockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwspringmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw213SpringmockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw213SpringmockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwspringmockito/Hw213SpringmockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwspringmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw213SpringmockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
